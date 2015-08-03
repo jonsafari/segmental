7f4cb7d8ce8be1e50dbeb0b02420fa596f1c2203
tuur <t.leeuwenberg@gmail.com>
2015-08-03 13:52:11 +0200

    Create uni-rnnlm-segment.py
    one iteration of segmentation using a unidirectional rnnlm.
    requires: rnnlm (tested on rnnlm-0.3e, http://www.rnnlm.org/)

---
+++ uni-rnnlm-segment.py
@@ -0,0 +1,88 @@
+# Word Segmentation using Unidirectional RNNLM: Tuur, Kata, Stalin
+import argparse
+import subprocess
+import sys
+import os
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Given a text file, and a prob-threshold trains a unidirectional RNNLM, and segments the text according to those places where the threshold is reached.')
+    parser.add_argument('text', metavar='text', type=str,
+                   help='The input text')
+    parser.add_argument('-threshold', metavar='-threshold', type=float,default = 0.5,
+                   help='The prob threshold (default=0.5)')
+    parser.add_argument('-rnnlm', metavar='rnnlm', type=str, default='./rnnlm',
+                   help='file path to the rnnlm program (default=./rnnlm)')
+    parser.add_argument('-output', metavar='output', type=str, default='segmented.txt',
+                   help='segmented output file (default: segmented.txt)')
+
+    args = parser.parse_args()  
+    
+    if not(os.path.isfile(args.rnnlm)):
+        sys.stderr.write('ERROR: Please provide a valid rnnlm path (option: -rnnlm rnnlmFilePath)\n')
+        exit()
+    if not(os.path.exists('tmp/')):
+        os.makedirs('tmp/')
+     
+    if os.path.exists('tmp/model.tmp'):
+        os.remove('tmp/model.tmp')
+     
+    # Split text in training and development texts, for RNNLM
+    sys.stderr.write('(1) Preparing for RNNLM training...\n')
+    trainFile = open('tmp/train.tmp', 'w')
+    devFile = open('tmp/dev.tmp','w')
+    lineNr = 0
+    with open(args.text) as f: 
+        for line in f.readlines():
+            if lineNr%5 == 0:
+                devFile.write(line)
+            else:
+                trainFile.write(line)
+            lineNr+=1
+    trainFile.close()
+    devFile.close()
+    
+
+    sys.stderr.write('(2) Training RNNLM...\n')
+    # Training RNNLM
+    with open('tmp/out.tmp','w') as output:
+        proc = subprocess.Popen([args.rnnlm,'-hidden','20', '-train', 'tmp/train.tmp', '-valid','tmp/dev.tmp','-rnnlm','tmp/model.tmp','-test', args.text,'-debug','2'], shell=False, stdout=subprocess.PIPE)
+    
+    # Segmenting using output from RNNLM
+    start = False
+    with open(args.output, 'w') as segmented:
+        for line in iter(proc.stdout.readline,''):
+
+                # if the training output part of stdout is over and it is actually outputting the probabilities
+                if start: 
+                    try:
+                        count, prob, word = line.split()
+                        prob = float(prob)
+                    except:
+                        sys.stderr.write(':'+line[:-1]+'\n')              
+                    if word=='</s>':
+                        segmented.write('\n')
+                    elif word!='<s>':
+                        if prob > args.threshold:
+                            segmented.write(word)
+                        else:
+                            segmented.write(' '+word)
+                        
+
+                    else:
+                        continue
+                  
+
+            
+                if line=='----------------------------------\n': # last line of training output
+                    sys.stderr.write('(3) Segmenting Text...\n')
+                    start=True    
+    
+    
+   
+    
+    
+    
+    
+    
