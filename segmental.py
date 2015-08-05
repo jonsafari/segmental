2a86bfd66ea55b3232b1d3386c0d4087462b4cf0
Jon Dehdari <jon@dehdari.org>
2015-08-05 13:48:52 +0200

    m: segmental.py: rm superfluous print statement

--- segmental.py
+++ segmental.py
@@ -9,7 +9,6 @@ import shelve
 ngram_order = 8 # replace with command-line argument
 
 ## open shelve db
-print("file: ", sys.argv[1])
 sh = shelve.open(sys.argv[1])
 
 ## Read in text to be segmented, from stdin
